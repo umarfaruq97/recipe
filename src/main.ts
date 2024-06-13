8aa05d53237b280321ca9114015713759b2f065d
Umar Faruq <umarfaruq@assetfindr.com>
2024-06-13 18:45:16 +0700

    feat: prisma and swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // use DocumentBuilder to create a new Swagger document configuration
+  const config = new DocumentBuilder()
+    .setTitle('Recipes API')
+    .setDescription('Recipes API description')
+    .setVersion('0.1')
+    .build();
+  // create a swagger document using the application instance and the document configuration
+  const document = SwaggerModule.createDocument(app, config);
+  // setup swagger module with the application instance and the swagger document
+  SwaggerModule.setup('api', app, document);
+  // start the app and listen for request on port 3000
   await app.listen(3000);
 }
 bootstrap();
